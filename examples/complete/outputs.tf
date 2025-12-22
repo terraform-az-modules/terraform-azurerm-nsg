cb993f12aaeea407c689a682c2f5d8a712e9fd23
Vedant Kalia <vedant.kalia@clouddrove.com>
2025-12-22 18:15:34 +0530

    fix: Added Support for nsg association with Subnet and Network Interface (#16)
    * fix: Added Support for nsg association with Subnet and Network Interface
    
    * fix: Updated modules and Refactored the code
    
    * fix: Resolved Gemini Comments

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,49 +1,24 @@
-output "subnet_ids" {
-  value       = module.subnet.default_subnet_id
-  description = "The ID of the Subnet. Changing this forces a new resource to be created."
-}
-
-output "subnet_name" {
-  value       = module.subnet.default_subnet_name
-  description = "The name of the subnet. Changing this forces a new resource to be created."
-}
-
 output "resource_group_name" {
-  description = "The name of the resource group in which the subnet is created in."
+  description = "The name of the resource group."
   value       = module.resource_group.resource_group_name
 }
 
 output "resource_group_location" {
+  description = "The resource group location."
   value       = module.resource_group.resource_group_location
-  description = "The name of the resource group in which the subnet is created in."
 }
 
 output "virtual_network_name" {
+  description = "The name of the virtual network."
   value       = module.vnet.vnet_name
-  description = "The name of the virtual network in which the subnet is created in."
-}
-
-output "address_prefixes" {
-  description = "The address prefixes for the subnet."
-  value       = module.subnet.default_subnet_address_prefixes
-}
-
-output "route_table_id" {
-  description = "The Route Table ID."
-  value       = module.subnet.route_table_id
-}
-
-output "route_table_associated_subnets" {
-  value       = module.subnet[*].route_table_associated_subnets
-  description = "The collection of Subnets associated with this route table."
 }
 
 output "security_group_id" {
   value       = module.network_security_group.id
-  description = "Specifies the name of the network security group. Changing this forces a new resource to be created."
+  description = "Network Security Group ID."
 }
 
 output "security_group_name" {
   value       = module.network_security_group.name
-  description = "The name of the resource group in which to create the network security group. Changing this forces a new resource to be created."
+  description = "Network Security Group name."
 }
--- main.tf
+++ main.tf
@@ -92,33 +92,22 @@ resource "azurerm_network_security_rule" "outbound" {
   }
 }
 
+##-----------------------------------------------------------------------------
+## Below resource will associate above created network security group to subnet.
+##-----------------------------------------------------------------------------
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet_association" {
+  count                     = var.enabled && var.subnet_association ? length(var.subnet_ids) : 0
+  subnet_id                 = element(var.subnet_ids, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg[0].id
+}
 
 ##-----------------------------------------------------------------------------
-# NSG Flow Logs – Enables logging of ingress and egress IP traffic for monitoring
+## Below resource will associate above created network security group to network interface(nic).
 ##-----------------------------------------------------------------------------
-resource "azurerm_network_watcher_flow_log" "nsg_flow_logs" {
-  count                = var.enabled && var.enable_flow_logs ? 1 : 0
-  name                 = format(var.resource_position_prefix ? "flow-logs-%s" : "%s-flow-logs", local.name)
-  enabled              = var.enabled
-  version              = var.flow_log_version
-  network_watcher_name = var.network_watcher_name
-  resource_group_name  = var.resource_group_name
-  target_resource_id   = azurerm_network_security_group.nsg[0].id
-  storage_account_id   = var.flow_log_storage_account_id
-  retention_policy {
-    enabled = var.flow_log_retention_policy_enabled
-    days    = var.flow_log_retention_policy_days
-  }
-  dynamic "traffic_analytics" {
-    for_each = var.enable_traffic_analytics ? [var.traffic_analytics_settings] : []
-    content {
-      enabled               = true
-      workspace_id          = traffic_analytics.value.log_analytics_workspace_resource_id
-      workspace_region      = traffic_analytics.value.workspace_region
-      workspace_resource_id = traffic_analytics.value.log_analytics_workspace_id
-      interval_in_minutes   = traffic_analytics.value.interval_in_minutes
-    }
-  }
+resource "azurerm_network_interface_security_group_association" "nsg_nic_association" {
+  count                     = var.enabled && var.nic_association ? length(var.nic_ids) : 0
+  network_interface_id      = element(var.nic_ids, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg[0].id
 }
 
 ##-----------------------------------------------------------------------------
@@ -128,7 +117,7 @@ resource "azurerm_monitor_diagnostic_setting" "nsg_diagnostic" {
   count                          = var.enabled && var.enable_diagnostic ? 1 : 0
   name                           = format(var.resource_position_prefix ? "nsg-diagnostic-log-%s" : "%s-nsg-diagnostic-log", local.name)
   target_resource_id             = azurerm_network_security_group.nsg[0].id
-  storage_account_id             = var.flow_log_storage_account_id
+  storage_account_id             = var.storage_account_id
   eventhub_name                  = var.eventhub_name
   eventhub_authorization_rule_id = var.eventhub_authorization_rule_id
   log_analytics_workspace_id     = var.log_analytics_workspace_id
