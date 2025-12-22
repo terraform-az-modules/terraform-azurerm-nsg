cea5d5a0b3dbc6811400db70ebd74093ea7688b1
Vedant <vedant.kalia@clouddrove.com>
2025-12-22 18:08:36 +0530

    fix: Resolved Gemini Comments

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -1,12 +1,10 @@
 provider "azurerm" {
   features {}
-  subscription_id = "1ac2caa4-336e-4daa-b8f1-0fbabe2d4b11"
 }
 
 provider "azurerm" {
   features {}
   alias           = "peer"
-  subscription_id = "1ac2caa4-336e-4daa-b8f1-0fbabe2d4b11"
 }
 
 ##-----------------------------------------------------------------------------
